66a6b5c6425b9c9b047dcb8e6773ba1fddd7710f
mtume <mtume2016@gmail.com>
2025-05-05 16:15:27 +0300

    Challenge 1: Greeter class

---
+++ src/Greeter.java
@@ -0,0 +1,10 @@
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+
+    public static void main(String[] args){
+        Greeter greeter_class= new Greeter();
+        greeter_class.greet();
+    }
+}
